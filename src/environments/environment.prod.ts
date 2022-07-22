dd1436473ec6b841950d513943975beba3755ea3
nienkek2101 <nienke.kapers@gmail.com>
2022-07-22 15:34:53 +0200

    delete key

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true
-  apiTrainers: "https://nk-experis-api.herokuapp.com/trainers",
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiTrainers: "https://nk-experis-api.herokuapp.com/trainers",
-  apiKey: "TbcVCytuhEaOVAXxxuLspw==",
-};
