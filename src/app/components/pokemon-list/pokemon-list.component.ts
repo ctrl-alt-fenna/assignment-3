13c32869b8534980deebd7c5bb1ac6ff30df66fc
fransijn <ikbenfenna@gmail.com>
2022-07-23 23:53:25 +0200

    FIX: Replaced EventEmitters, improved legibility

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
+import { Component, Input, OnInit,} from '@angular/core'
 import { Pokemon } from 'src/app/models/pokemon.model'
 @Component({
   selector: 'app-pokemon-list',
@@ -9,24 +9,8 @@ export class PokemonListComponent implements OnInit {
     public limit:number = 50
     public current:number = 0
     public offset:number = this.current
-    @Input() pokemons: Pokemon[] = []
-    @Input() stats?: string[]
-    @Input() abilities?: string[]
-    @Input() collectedPokemons?: string[]
-    @Output() updatePokemons = new EventEmitter<Pokemon>()
+    @Input() pokemons!:Pokemon[]
 
   constructor() { }
-  updatePokemon(pokemon_:Pokemon)
-  {
-    this.updatePokemons.emit(pokemon_)
-  }
-  // onViewStats(pokemon:Pokemon)
-  // {
-  //   this.viewStats.emit(pokemon);
-  // }y
-  // onViewAbilities(pokemon:Pokemon)
-  // {
-  //   this.viewAbilities.emit(pokemon);
-  // }
   ngOnInit(): void {  }
 }
