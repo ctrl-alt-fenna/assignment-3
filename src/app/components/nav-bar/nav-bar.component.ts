67daad8e1e9c5e60afaf6a1726da8e13086ea86e
fransijn <ikbenfenna@gmail.com>
2022-07-25 11:26:31 +0200

    Fix logout

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { StorageKeys } from 'src/app/enums/storage-keys.enum';
 import { UserService } from 'src/app/services/user.service';
-import { StorageUtil } from '../../utils/storage.util';
 
 @Component({
   selector: 'app-nav-bar',
@@ -11,19 +9,9 @@ import { StorageUtil } from '../../utils/storage.util';
 })
 export class NavBarComponent implements OnInit {
 
-  public isLoggedIn: boolean = false;
-
-  // get loggedIn(): boolean {
-  //   return this.isLoggedIn
-  // } 
-
-  toggleLoggedIn(): boolean {
-    if (this.userService.trainer) {
-      return this.isLoggedIn = true;
-    } else {
-      return this.isLoggedIn = false;
-    }
-  }
+  get loggedIn(): boolean {
+    return (this.userService.trainer !== undefined)
+  } 
 
   constructor( 
     private readonly router: Router,
@@ -33,10 +21,6 @@ export class NavBarComponent implements OnInit {
   handleLogout() {
     this.userService.removeTrainer()
     this.router.navigateByUrl('/login')
-    this.toggleLoggedIn()
-    console.log(this.isLoggedIn)
-    // this.isLoggedIn = false;
-    // console.log(this.isLoggedIn)
   }
 
   ngOnInit(): void {
